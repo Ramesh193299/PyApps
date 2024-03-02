d20e937681bd0e06264c3caf3c6dda390f1f934c
Ramesh Danda <ramesh.danda@gmail.com>
2024-03-01 22:05:42 -0500

    first

--- PythonSnowflake.py
+++ PythonSnowflake.py
@@ -1,3 +1,5 @@
+#This is first Python Prgram 
+
 import snowflake.connector
 import pandas as pd
 conn = snowflake.connector.connect(user='ramesh1980', password='Prannu@2009', account='swb55287.us-east-1')
